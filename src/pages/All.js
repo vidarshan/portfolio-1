36187f2ceb0256b94c67756c04ef987472c054c0
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2021-08-20 22:32:24 +0530

    Adding under construction

---
+++ src/pages/All.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const All = () => {
+  return (
+    <div className='under-constrction'>
+      <img src='../wheel.gif' alt='' />
+      <div className='text-xl text-weight-600 text-align-center'>
+        Under Development
+      </div>
+      <div className='text-sm text-weight-500 text-align-center'>
+        Will be launching soon
+      </div>
+    </div>
+  );
+};
+
+export default All;
