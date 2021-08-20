d2c411aeec9aa245ab1cabaf79ced828928a8835
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2021-08-20 22:34:34 +0530

    Changes

--- src/pages/All.js
+++ src/pages/All.js
@@ -3,7 +3,7 @@ import React from 'react';
 const All = () => {
   return (
     <div className='under-constrction'>
-      <img src='../wheel.gif' alt='' />
+      <img src='../wheel.gif' alt='' className='construction-icon' />
       <div className='text-xl text-weight-600 text-align-center'>
         Under Development
       </div>
