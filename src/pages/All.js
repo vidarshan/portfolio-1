436cea58f1dad44003b4309978db961dbb77624f
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2021-09-03 21:13:46 +0530

    Added home screen

--- src/pages/All.js
+++ src/pages/All.js
@@ -2,15 +2,80 @@ import React from 'react';
 
 const All = () => {
   return (
-    <div className='under-constrction'>
-      <img src='../wheel.gif' alt='' className='construction-icon' />
-      <div className='text-xl text-weight-600 text-align-center'>
-        Under Development
-      </div>
-      <div className='text-sm text-weight-500 text-align-center'>
-        Will be launching soon
-      </div>
-    </div>
+    <>
+      <header className='header'>
+        <ul class='nav-icon-list'>
+          <li>
+            <img
+              className='nav-icon'
+              src={require('../img/home.svg').default}
+              alt=''
+            />
+          </li>
+          <li>
+            <img
+              className='nav-icon'
+              src={require('../img/profile.svg').default}
+              alt=''
+            />
+          </li>
+          <li>
+            <img
+              className='nav-icon'
+              src={require('../img/projects.svg').default}
+              alt=''
+            />
+          </li>
+
+          <li>
+            <img
+              className='nav-icon'
+              src={require('../img/achievement.svg').default}
+              alt=''
+            />
+          </li>
+          <li>
+            <img
+              className='nav-icon'
+              src={require('../img/contact.svg').default}
+              alt=''
+            />
+          </li>
+        </ul>
+      </header>
+      <section class='home'>
+        <div class='intro-text-container'>
+          <div
+            class='
+            text-xl text-weight-600 text-align-right
+            spacing-1 spacing-right-3
+          '>
+            Hello.
+          </div>
+          <div
+            class='
+            text-xl text-weight-600 text-align-right
+            spacing-1 spacing-right-3
+          '>
+            I'm Vidarshan;
+          </div>
+
+          <div class='text-cursor-same-line'>
+            <div class='text-xl text-weight-600 text-align-right'>
+              A Software Engineer
+            </div>
+            <div
+              class='
+              text-xl text-weight-600 text-align-right
+              blinking-cursor
+              spacing-right-3
+            '>
+              |
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
