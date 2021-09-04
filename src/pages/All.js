c06bb2dcd9c78e2246f6299837182957d1f6449b
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2021-09-04 12:34:21 +0530

    Profile basic ui

--- src/pages/All.js
+++ src/pages/All.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const All = () => {
+  function myFunction() {
+    var element = document.body;
+    element.classList.toggle('dark-mode');
+  }
+
   return (
     <>
       <header className='header'>
@@ -43,6 +49,10 @@ const All = () => {
           </li>
         </ul>
       </header>
+      <img
+        onClick={myFunction}
+        className='switch-theme'
+        src={require('../img/sun.svg').default}></img>
       <section class='home'>
         <div class='intro-text-container'>
           <div
@@ -71,6 +81,40 @@ const All = () => {
               spacing-right-3
             '>
               |
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='profile'>
+        <div className='heading text-xl text-weight-600'>Profile</div>
+        <div className='profile-row'>
+          <div className='biography-col'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+            et? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+            delectus eius rem consequatur? Magnam perferendis qui consectetur in
+            ullam quae laboriosam nemo. Nihil eligendi numquam voluptas eum cum
+            nostrum quam qui, at ut obcaecati enim adipisci quos, animi vitae
+            doloremque libero reprehenderit, voluptatem corrupti illum molestiae
+            soluta? Exercitationem sunt, sit iste enim laborum dolor tenetur
+            quidem odio libero sed neque magnam, cupiditate nemo expedita fugiat
+            temporibus. Eos quo, commodi tempore maiores iure id dignissimos
+            quam provident, inventore esse suscipit in incidunt hic quisquam
+            officiis quae nemo consequatur dolor libero! Blanditiis iste aliquid
+            eveniet nisi consequuntur reprehenderit! Similique reprehenderit
+            saepe eligendi libero aperiam eveniet soluta illo nobis natus totam
+            distinctio quasi consequuntur, aut quisquam necessitatibus enim
+            laboriosam accusantium numquam! Maiores harum repellendus eligendi
+            atque ratione aperiam perferendis, excepturi rerum incidunt vero.
+            Hic nisi, consequuntur dicta, corrupti veritatis explicabo vitae
+            tenetur nesciunt laborum minus quasi laudantium neque voluptatibus
+            earum sapiente aut ad.
+          </div>
+          <div className='timeline-col'>
+            <div className='text-md'>Journey as a Developer</div>
+            <div className='timeline-items-row'>
+              <div className='timeline-item'></div>
+              <div className='timeline-item'></div>
+              <div className='timeline-item'></div>
             </div>
           </div>
         </div>
